3b66902184fcfef64cf807f1ef57acdb055a9849
IGORECHEKST <165463152+IGORECHEKST@users.noreply.github.com>
2025-04-26 09:09:18 +0300

    my 4 solution

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -4,34 +4,32 @@ import core.mate.academy.model.Bulldozer;
 import core.mate.academy.model.Excavator;
 import core.mate.academy.model.Machine;
 import core.mate.academy.model.Truck;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
-public class MachineServiceImpl<T extends Machine> implements MachineService<T> {
+public class MachineServiceImpl implements MachineService<Machine> {
 
     private final BulldozerProducer bulldozerProducer = new BulldozerProducer();
     private final ExcavatorProducer excavatorProducer = new ExcavatorProducer();
     private final TruckProducer truckProducer = new TruckProducer();
 
     @Override
-    public List<T> getAll(Class<? extends Machine> type) {
-        List<T> list = Collections.emptyList();
-        if (type.equals(Bulldozer.class)) {
-            list = (List<T>) bulldozerProducer.get();
+    public List<Machine> getAll(Class<? extends Machine> type) {
+        if (type == Bulldozer.class) {
+            return new ArrayList<>(bulldozerProducer.get());
         }
-        if (type.equals(Excavator.class)) {
-            list = (List<T>) excavatorProducer.get();
+        if (type == Excavator.class) {
+            return new ArrayList<>(excavatorProducer.get());
         }
-        if (type.equals(Truck.class)) {
-            list = (List<T>) truckProducer.get();
+        if (type == Truck.class) {
+            return new ArrayList<>(truckProducer.get());
         }
-        return list;
+        return new ArrayList<Machine>();
     }
 
     @Override
     public void fill(List<? super Machine> machines, Machine value) {
-        int size = machines.size();
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < machines.size(); i++) {
             machines.set(i, value);
         }
     }
