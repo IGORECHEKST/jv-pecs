fa8ca7c1e038e560db0b4bb4ddab579877a2527b
IGORECHEKST <165463152+IGORECHEKST@users.noreply.github.com>
2025-04-26 09:21:37 +0300

    my 5 solution

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -7,35 +7,35 @@ import core.mate.academy.model.Truck;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MachineServiceImpl implements MachineService<Machine> {
+public class MachineServiceImpl<T extends Machine> implements MachineService<T> {
 
     private final BulldozerProducer bulldozerProducer = new BulldozerProducer();
     private final ExcavatorProducer excavatorProducer = new ExcavatorProducer();
     private final TruckProducer truckProducer = new TruckProducer();
 
     @Override
-    public List<Machine> getAll(Class<? extends Machine> type) {
+    public List<T> getAll(Class<? extends T> type) {
         if (type == Bulldozer.class) {
-            return new ArrayList<>(bulldozerProducer.get());
+            return (List<T>) new ArrayList<>(bulldozerProducer.get());
         }
         if (type == Excavator.class) {
-            return new ArrayList<>(excavatorProducer.get());
+            return (List<T>) new ArrayList<>(excavatorProducer.get());
         }
         if (type == Truck.class) {
-            return new ArrayList<>(truckProducer.get());
+            return (List<T>) new ArrayList<>(truckProducer.get());
         }
-        return new ArrayList<Machine>();
+        return (List<T>) new ArrayList<Machine>();
     }
 
     @Override
-    public void fill(List<? super Machine> machines, Machine value) {
+    public void fill(List<? super T> machines, T value) {
         for (int i = 0; i < machines.size(); i++) {
             machines.set(i, value);
         }
     }
 
     @Override
-    public void startWorking(List<? extends Machine> machines) {
+    public void startWorking(List<? extends T> machines) {
         for (Machine machine : machines) {
             machine.doWork();
         }
